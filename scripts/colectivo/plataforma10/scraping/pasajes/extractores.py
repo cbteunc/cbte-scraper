4a6f0a0678c4af6819e6e49f452d9dd10704f7e0
ArielIvanLeiva <ariel.leiva@mi.unc.edu.ar>
2026-06-25 13:14:03 -0300

    Actualización de las clases del scrapping de plataforma10.

--- scripts/colectivo/plataforma10/scraping/pasajes/extractores.py
+++ scripts/colectivo/plataforma10/scraping/pasajes/extractores.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 def wait_for_visible_text(driver, timeout=5):
     # Esperar a que el contenido informativo esté presente
     WebDriverWait(driver, timeout).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".SearchResultClient_results-data-content__k6rav"))
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".SearchResultClient-module__Avjk-G__results-container"))
     )
 
 
@@ -14,24 +14,24 @@ def wait_for_visible_text(driver, timeout=5):
 def wait_for_card_container(driver, timeout=40):
     # Esperar hasta que las tarjetas estén presentes
     WebDriverWait(driver, timeout).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".SearchResultClient_service-group-services__2TIq7"))
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".SearchResultClient-module__Avjk-G__service-group"))
     )
 
 def find_card_container(driver):
-    return driver.find_element(By.CSS_SELECTOR, ".SearchResultClient_service-group-services__2TIq7")
+    return driver.find_element(By.CSS_SELECTOR, ".SearchResultClient-module__Avjk-G__service-group")
 
 def find_card_list(cards_container):
-    return cards_container.find_elements(By.CSS_SELECTOR, ".GroupedSearchResultCard_card__r7pxV")
+    return cards_container.find_elements(By.CSS_SELECTOR, ".GroupedSearchResultCard-module__jXaVgq__card")
 
 def find_imagen_empresa(card):
-    return card.find_element(By.CSS_SELECTOR, "div.GroupedSearchResultCard_header__7H134 img")
+    return card.find_element(By.CSS_SELECTOR, "div.GroupedSearchResultCard-module__jXaVgq__header img")
 
 def find_span_nombre_empresa(card):
-    return card.find_element(By.CSS_SELECTOR, ".GroupedSearchResultCard_companyNameFallback__lUPli")
+    return card.find_element(By.CSS_SELECTOR, ".GroupedSearchResultCard-module__jXaVgq__companyNameFallback")
 
 # --- Asientos ---
 def find_seat_type_container(card):
-    return card.find_element(By.CSS_SELECTOR, ".GroupedSearchResultCard_qualities__QoudX")
+    return card.find_element(By.CSS_SELECTOR, ".GroupedSearchResultCard-module__jXaVgq__qualities")
 
 def find_seat_type_list(card):
     seat_types_container = find_seat_type_container(card)
@@ -40,8 +40,8 @@ def find_seat_type_list(card):
     return seat_types_container.find_elements(By.XPATH, "./*")
 
 def find_seat_type_element(seat_type_button):
-    return seat_type_button.find_element(By.CSS_SELECTOR, ".GroupedSearchResultCard_qualityName__a1ZT5")
+    return seat_type_button.find_element(By.CSS_SELECTOR, ".GroupedSearchResultCard-module__jXaVgq__qualityName")
 
 # --- Precios ---
 def find_price_element(seat_type_button):
-    return seat_type_button.find_element(By.CSS_SELECTOR, ".GroupedSearchResultCard_qualityAmount__bBHMb")
+    return seat_type_button.find_element(By.CSS_SELECTOR, ".GroupedSearchResultCard-module__jXaVgq__qualityAmount")
